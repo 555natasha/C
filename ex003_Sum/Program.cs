3f7c6c1d4102a1f3219d43dc6a4847b64cc7010b
NatASha <nash_ka@mail.ru>
2023-12-13 00:56:55 +0300

    В примере ex003_Sum применили генератор случайных чисел

--- ex003_Sum/Program.cs
+++ ex003_Sum/Program.cs
@@ -1,7 +1,15 @@
 ﻿//Console.Write("Введите первое число ");
-int first_ch = 3;
+//int first_ch = 3;
 //Console.Write("Введите второе число ");
-int second_ch = 8;
-int result = first_ch + second_ch;
+//int second_ch = 8;
+int numberA = new Random().Next(1, 10); // 1 2 3 4 5 6 7 8 9
+Console.Write("число numberA ");
+Console.Write(numberA);
+int numberB = new Random().Next(1, 10);
+Console.Write(" число numberB ");
+Console.WriteLine(numberB);
+
+//int result = first_ch + second_ch;
+int result = numberA + numberB;
 Console.WriteLine("Сумма равна ");
 Console.WriteLine(result);
