7d0b24bee02cdd14694e68ff3dcf59d142d361c4
NatASha <nash_ka@mail.ru>
2023-12-13 00:28:12 +0300

    Создали пример ex003_Sum

---
+++ ex003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿//Console.Write("Введите первое число ");
+int first_ch = 3;
+//Console.Write("Введите второе число ");
+int second_ch = 8;
+int result = first_ch + second_ch;
+Console.WriteLine("Сумма равна ");
+Console.WriteLine(result);
